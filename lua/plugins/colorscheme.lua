eb57d3f5058e4cc1e59db7b383c0dfb3b74b73e9
sagevik <rsagevik@gmail.com>
2025-03-23 14:42:28 +0100

    initial organization and cleanup

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+	"rebelot/kanagawa.nvim",
+	priority = 1000,
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require("kanagawa").setup({
+			styles = {
+				comments = { italic = false },
+			},
+		})
+
+		-- Load the colorscheme here.
+		-- Like many other themes, this one has different styles, and you could load
+		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+		vim.cmd.colorscheme("kanagawa-dragon")
+	end,
+}
