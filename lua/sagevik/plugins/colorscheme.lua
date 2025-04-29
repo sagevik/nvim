0b0a907920b6cb6e1bcfced123639bc309858266
sagevik <rsagevik@gmail.com>
2025-04-29 19:39:43 +0200

    add colorschemes

--- lua/sagevik/plugins/colorscheme.lua
+++ lua/sagevik/plugins/colorscheme.lua
@@ -1,18 +1,43 @@
 return {
-	"rebelot/kanagawa.nvim",
-	priority = 1000,
-	config = function()
-		---@diagnostic disable-next-line: missing-fields
-		require("kanagawa").setup({
-			transparent = true,
-			styles = {
-				comments = { italic = false },
-			},
-		})
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		config = function()
+			---@diagnostic disable-next-line: missing-fields
+			require("kanagawa").setup({
+				transparent = true,
+				styles = {
+					comments = { italic = false },
+				},
+			})
 
-		-- Load the colorscheme here.
-		-- Like many other themes, this one has different styles, and you could load
-		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-		vim.cmd.colorscheme("kanagawa-dragon")
-	end,
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("kanagawa-dragon")
+		end,
+	},
+	{
+		"vague2k/vague.nvim",
+		config = function()
+			require("vague").setup({
+				transparent = true,
+			})
+			vim.cmd("colorscheme vague")
+			vim.cmd(":hi statusline guibg=NONE")
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {
+					italic = false,
+				},
+			})
+			vim.cmd("colorscheme rose-pine")
+		end,
+	},
 }
