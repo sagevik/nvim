42f97a929b11b1ac2ca43fdade8bc0309c0eea0e
sagevik <rsagevik@gmail.com>
2025-05-01 16:47:31 +0200

    remove call to deprecated function

--- lua/sagevik/lazy.lua
+++ lua/sagevik/lazy.lua
@@ -8,26 +8,24 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 })
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup(
-    {
-        spec = { import = "sagevik.plugins" },
-        change_detection = { enabled = false },
-    }
-)
+require("lazy").setup({
+	spec = { import = "sagevik.plugins" },
+	change_detection = { enabled = false },
+})
