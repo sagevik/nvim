9916ecbf7cc0afbdf85a97624cfa7ac396f80a8a
sagevik <rsagevik@gmail.com>
2025-05-02 23:14:56 +0200

    add enable/disable flag

--- lua/sagevik/plugins/project.lua
+++ lua/sagevik/plugins/project.lua
@@ -1,10 +1,15 @@
-return {
-    "ahmedkhalf/project.nvim",
-    init = function()
-        require("project_nvim").setup {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    end
-}
+local enabled = false
+if enabled then
+	return {
+		"ahmedkhalf/project.nvim",
+		init = function()
+			require("project_nvim").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	}
+else
+	return {}
+end
