b5de6cf828dfe588a9f155e6e46a59f71fd84382
sagevik <rsagevik@gmail.com>
2025-06-05 12:09:50 +0200

    change fidget settings in lsp.lua

--- lua/sagevik/plugins/lsp.lua
+++ lua/sagevik/plugins/lsp.lua
@@ -15,7 +15,21 @@ return {
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
 		-- Useful status updates for LSP.
-		{ "j-hui/fidget.nvim", opts = {} },
+		{
+			"j-hui/fidget.nvim",
+			opts = {
+				progress = {
+					display = {
+						done_icon = "✓", -- Icon shown when all LSP progress tasks are complete
+					},
+				},
+				notification = {
+					window = {
+						winblend = 0, -- Background color opacity in the notification window
+					},
+				},
+			},
+		},
 	},
 	config = function()
 		-- Brief aside: **What is LSP?**
