0f4ea2dcb7c91c508256e3cbcb9b563e4effed0e
sagevik <rsagevik@gmail.com>
2025-07-29 20:21:20 +0200

    enable ruff format and organize imports

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -37,7 +37,7 @@ return {
 			sh = { "shfmt" },
 			bash = { "shfmt" },
 			go = { "goimports", "gofmt" },
-			-- python = { "ruff_format", "ruff_organize_imports" },
+			python = { "ruff_format", "ruff_organize_imports" },
 			-- rust = { "rustfmt" }, -- Deprecated, should be installed via rustup
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
