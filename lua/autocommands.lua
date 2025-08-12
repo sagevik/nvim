a39b068365983208e7f2a764475147331f7b3c8f
sagevik <rsagevik@gmail.com>
2025-08-12 07:44:13 +0200

    temp fix: restart lsp on python save

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -11,3 +11,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*.py",
+	callback = function()
+		vim.cmd("LspRestart pyright")
+		vim.cmd("LspRestart pylsp")
+	end,
+	desc = "Restart Pyright and pylsp after saving a Python file",
+})
