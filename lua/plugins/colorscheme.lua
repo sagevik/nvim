2c8bad267a1e1853c01fedd9f15325c04d584ddf
sagevik <rsagevik@gmail.com>
2025-09-21 17:53:24 +0200

    disable colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,17 @@
 return {
-	{
-		"rebelot/kanagawa.nvim",
-		config = function()
-			require("kanagawa").setup({
-				compile = true,
-				transparent = true,
-			})
-			-- vim.cmd("colorscheme kanagawa-dragon")
-		end,
-		build = function()
-			vim.cmd("KanagawaCompile")
-		end,
-	},
+	-- {
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	config = function()
+	-- 		require("kanagawa").setup({
+	-- 			compile = true,
+	-- 			transparent = true,
+	-- 		})
+	-- 		-- vim.cmd("colorscheme kanagawa-dragon")
+	-- 	end,
+	-- 	build = function()
+	-- 		vim.cmd("KanagawaCompile")
+	-- 	end,
+	-- },
 	{
 		"vague2k/vague.nvim",
 		config = function()
@@ -22,23 +22,23 @@ return {
 			vim.cmd(":hi statusline guibg=NONE")
 		end,
 	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				disable_background = true,
-				styles = {
-					italic = false,
-				},
-			})
-			-- vim.cmd.colorscheme("rose-pine")
-		end,
-	},
-	{
-		"metalelf0/base16-black-metal-scheme",
-		config = function()
-			-- vim.cmd("colorscheme base16-black-metal-gorgoroth")
-		end,
-	},
+	-- {
+	-- 	"rose-pine/neovim",
+	-- 	name = "rose-pine",
+	-- 	config = function()
+	-- 		require("rose-pine").setup({
+	-- 			disable_background = true,
+	-- 			styles = {
+	-- 				italic = false,
+	-- 			},
+	-- 		})
+	-- 		-- vim.cmd.colorscheme("rose-pine")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"metalelf0/base16-black-metal-scheme",
+	-- 	config = function()
+	-- 		-- vim.cmd("colorscheme base16-black-metal-gorgoroth")
+	-- 	end,
+	-- },
 }
