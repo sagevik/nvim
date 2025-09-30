4fd0f3fb4eb2047b88eb172e9d29ef3187f73639
sagevik <rsagevik@gmail.com>
2025-09-30 09:59:09 +0200

    options and keybinds mostly

--- init.lua
+++ init.lua
@@ -1,89 +1,211 @@
 vim.g.mapleader = " "
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes"
-vim.opt.winborder = "rounded"
-vim.opt.cursorline = true
-vim.opt.wrap = false
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.g.maplocalleader = " "
 
-vim.opt.swapfile = false
-
--- vim.keymap.set('n', '<leader>o', '<cmd>update<CR> <cmd>source<CR>')
--- vim.keymap.set('n', '<leader>w', '<cmd>write<CR>')
--- vim.keymap.set('n', '<leader>q', '<cmd>quit<CR>')
+require("options")
 
 vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-	{ src = "https://github.com/nvim-mini/mini.pick" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
+  { src = "https://github.com/vague2k/vague.nvim" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/mason-org/mason.nvim" },
+  { src = "https://github.com/ibhagwan/fzf-lua" },
 })
 
--- require "nvim-lspconfig".setup()
 require "mason".setup()
-require "oil".setup()
-vim.keymap.set('n', '-', ":Oil<CR>")
-require "mini.pick".setup()
-vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
-vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+require("oil").setup({
+  lsp_file_methods = {
+    enabled = true,
+    timeout_ms = 1000,
+    autosave_changes = true,
+  },
+  float = {
+    max_width = 0.7,
+    max_height = 0.6,
+    border = "rounded",
+  },
+})
 
+-- Colorscheme
 require "vague".setup({ transparent = true })
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
 
 require "nvim-treesitter.configs".setup({
-	ensure_installed = { "lua", "go", "python" },
-	highlight = { enable = true },
+  ensure_installed = { "lua", "go", "python" },
+  highlight = { enable = true },
 })
 
-vim.lsp.enable({ "lua_ls", "pyright", "gopls" })
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+-- LSP config
+vim.lsp.enable({
+  "lua_ls",
+  "basedpyright",
+  "gopls",
+  "ruff"
+})
 
--- auto complete
+
+-- Autocomplete
 vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client:supports_method('textDocument/completion') then
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-		end
-	end,
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
 })
 vim.cmd("set completeopt+=noselect")
 
+-- Diagnostics
 vim.diagnostic.config({
-	virtual_text = true,
+  virtual_text = true,
 })
 
 
+-- Autocommands
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
-	callback = function()
-		vim.hl.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	-- pattern = "*.py",
-	callback = function()
-		vim.lsp.buf.format()
-	end,
-	desc = "Format buffer before save",
+  -- pattern = "*.py",
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+  desc = "Format buffer before save",
 })
 
 vim.api.nvim_create_autocmd("BufWritePost", {
-	pattern = "*.py",
-	callback = function()
-		-- vim.lsp.buf.format()
-		vim.cmd("LspRestart pyright")
-		vim.cmd("LspRestart pylsp")
-	end,
-	desc = "Restart Pyright and pylsp after saving a Python file",
+  pattern = "*.py",
+  callback = function()
+    vim.cmd("LspRestart pyright")
+    vim.cmd("LspRestart pylsp")
+  end,
+  desc = "Restart Pyright and pylsp after saving a Python file",
 })
 
-vim.keymap.set('n', '<C-j>', '<cmd>cnext<CR>')
-vim.keymap.set('n', '<C-k>', '<cmd>cprev<CR>')
+-- keymaps
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", "bp", "<cmd>put<CR>", opts)
+vim.keymap.set("n", "ap", "<cmd>put!<CR>", opts)
+
+--  Use CTRL+<hjkl> to switch between windows
+-- vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+-- vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+-- vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+-- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Navigate quickfix list
+vim.keymap.set('n', '<C-j>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-k>', '<cmd>cprev<CR>zz')
+
+-- vim.keymap.set('n', '<leader>o', '<cmd>update<CR> <cmd>source<CR>')
+-- vim.keymap.set('n', '<leader>w', '<cmd>write<CR>')
+-- vim.keymap.set('n', '<leader>q', '<cmd>quit<CR>')
+
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+vim.keymap.set('n', '<leader>E', ":Oil --float<CR>")
+
+vim.keymap.set("n", "<leader>a",
+  function() vim.fn.setqflist({ { filename = vim.fn.expand("%"), lnum = 1, col = 1, text = vim.fn.expand("%"), } }, "a") end,
+  { desc = "Add current file to QuickFix" })
+local opts = { noremap = true, silent = true }
+
+-- vim.keymap.set('n', "-", "<cmd>Oil --float<CR>", { desc="Open in Oil" })
+vim.keymap.set("n", "gl", function()
+  vim.diagnostic.open_float()
+end, { desc = "Open diagnostics in float" })
+
+-- Disable the spacebar key's default behavior in Normal and Visual modes
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- vim.keymap.set("n", "<C-g>", "<Nop>", { silent = true })
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+-- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+--  See `:help wincmd` for a list of all window commands
+--  Use CTRL+<hjkl> to switch between windows
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Switch buffer with shift + h/l
+vim.keymap.set("n", "<leader>l", ":bnext<CR>", opts)
+vim.keymap.set("n", "<leader>h", ":bprev<CR>", opts)
+
+-- Delete single character without copying into register
+vim.keymap.set("n", "x", '"_x', opts)
+
+-- Keep last yanked when pasting
+vim.keymap.set("v", "p", '"_dP', opts)
+
+-- vim.keymap.set("n", "<leader>p", "<cmd>put<CR>", opts)
+-- vim.keymap.set("n", "<leader>P", "<cmd>put!<CR>", opts)
+vim.keymap.set("n", "bp", "<cmd>put<CR>", opts)
+vim.keymap.set("n", "ap", "<cmd>put!<CR>", opts)
+
+-- Vertical scroll and center
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+
+-- Jump and center
+vim.keymap.set("n", "<C-o>", "<C-o>zz", opts)
+vim.keymap.set("n", "<C-i>", "<C-i>zz", opts)
+
+-- Find and center
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Resize with arrows
+vim.keymap.set("n", "<Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<Right>", ":vertical resize +2<CR>", opts)
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+-- Better J behavior
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
+
+-- Move selected text up and down with shift + j/k
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Clear stuck previews and restore LSP diagnostics
+vim.keymap.set("n", "<leader>cp", function()
+  vim.api.nvim_buf_clear_namespace(0, -1, 0, -1)
+  vim.diagnostic.show(nil, 0, nil, { virtual_text = true })
+end, { desc = "Clear stuck LSP previews and restore diagnostics" })
+
+-- vim.keymap.set("n", "K", function()
+--   vim.lsp.buf.hover({
+--     border = "rounded",
+--     max_height = 20,
+--     max_width = 130,
+--     close_events = { "CursorMoved", "BufLeave", "WinLeave", "LSPDetach" },
+--   })
+-- end)
