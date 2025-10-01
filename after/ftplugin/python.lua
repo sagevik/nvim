40e6777ba603391e3ad4633656328c2d8119e709
sagevik <rsagevik@gmail.com>
2025-10-01 23:37:23 +0200

    configure pyslp and ruff

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,11 +1,52 @@
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
+local disable_capabilities = function(client, bufnr)
+  client.server_capabilities.referencesProvider = false
+  client.server_capabilities.renameProvider = false
+  client.server_capabilities.documentSymbolProvider = false
+  client.server_capabilities.typeDefinitionProvider = false
+  client.server_capabilities.diagnosticProvider = false
+end
+
+vim.lsp.config("pylsp", {
+  on_attach = {
+    disable_capabilities,
+  },
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          enabled = true,
+          ignore = { "E203", "W503" },
+          maxLineLength = 120,
+        },
+        pyflakes = { enabled = false },
+        flake8 = { enabled = false },
+        pylint = { enabled = false },
+        rope_autoimport = { enabled = true },
+        rope_completion = { enabled = false },
+        jedi_completion = { enabled = false },
+        jedi_definition = { enabled = false },
+      },
+    },
+  },
+})
+
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   pattern = "*.py",
+--   callback = function()
+--     vim.cmd("LspRestart basedpyright")
+--     vim.cmd("LspRestart pylsp")
+--   end,
+--   desc = "Restart Pyright and pylsp after saving a Python file",
+-- })
+--
+-- vim.api.nvim_create_augroup("ruff_organize_imports", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePost", {
+  -- group = "ruff_organize_imports",
   pattern = "*.py",
   callback = function()
-    vim.cmd("LspRestart pyright")
-    vim.cmd("LspRestart pylsp")
+    vim.fn.execute("silent !ruff check --fix --select I %")
   end,
-  desc = "Restart Pyright and pylsp after saving a Python file",
 })
