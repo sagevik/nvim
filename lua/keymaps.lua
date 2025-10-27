0aa9ab05755429f3a46a1ff94e56e0ad7ecaf116
sagevik <rsagevik@gmail.com>
2025-10-27 15:53:34 +0100

    change resize buffer keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -78,11 +78,15 @@ vim.keymap.set("n", "<C-i>", "<C-i>zz", opts)
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Resize with arrows
-vim.keymap.set("n", "<Up>", ":resize -2<CR>", opts)
-vim.keymap.set("n", "<Down>", ":resize +2<CR>", opts)
-vim.keymap.set("n", "<Left>", ":vertical resize -2<CR>", opts)
-vim.keymap.set("n", "<Right>", ":vertical resize +2<CR>", opts)
+-- Resize buffer windows
+-- vim.keymap.set("n", "<Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<M-k>", ":resize -2<CR>", opts)
+-- vim.keymap.set("n", "<Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<M-j>", ":resize +2<CR>", opts)
+-- vim.keymap.set("n", "<Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<M-h>", ":vertical resize -2<CR>", opts)
+-- vim.keymap.set("n", "<Right>", ":vertical resize +2<CR>", opts)
+vim.keymap.set("n", "<M-l>", ":vertical resize +2<CR>", opts)
 
 -- Stay in indent mode
 vim.keymap.set("v", "<", "<gv", opts)
