dbe0e8c9c8558f60f934e9b53f1c6b2a8609c94c
sagevik <rsagevik@gmail.com>
2025-11-08 08:06:30 +0100

    add autocommands

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -15,7 +15,26 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
   callback = function()
-    vim.hl.on_yank()
+    vim.hl.on_yank({ timeout = 300, visual = true })
+  end,
+})
+
+-- Open help in vertical split
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "help",
+  command = "wincmd L",
+})
+
+-- Auto resize splits when resizing terminal window
+vim.api.nvim_create_autocmd("VimResized", {
+  command = "wincmd =",
+})
+
+-- No auto continue comments on new line
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("NoAutoComment", {}),
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
 })
 
@@ -48,3 +67,52 @@ vim.api.nvim_create_autocmd("CursorHoldI", {
     end)
   end,
 })
+
+-- Show cursorline only in active window enable
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("ActiveCursorline", { clear = true }),
+  callback = function()
+    vim.opt_local.cursorline = true
+  end,
+})
+
+-- Show cursorline only in active window disable
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+  group = "ActiveCursorline",
+  callback = function()
+    vim.opt_local.cursorline = false
+  end,
+})
+
+-- IDE like highlight when stopping cursor
+-- vim.api.nvim_create_autocmd("CursorMoved", {
+--   group = vim.api.nvim_create_augroup("LspReferenceHighlight", { clear = true }),
+--   desc = "Highlight references under cursor",
+--   callback = function()
+--     -- Only run if the cursor is not in insert mode
+--     if vim.fn.mode() ~= "i" then
+--       local clients = vim.lsp.get_clients({ bufnr = 0 })
+--       local supports_highlight = false
+--       for _, client in ipairs(clients) do
+--         if client.server_capabilities.documentHighlightProvider then
+--           supports_highlight = true
+--           break -- Found supporting client, not need to check others
+--         end
+--       end
+--
+--       if supports_highlight then
+--         vim.lsp.buf.clear_references()
+--         vim.lsp.buf.document_highlight()
+--       end
+--     end
+--   end,
+-- })
+--
+-- -- IDE like highlight when stopping cursor
+-- vim.api.nvim_create_autocmd("CursorMovedI", {
+--   group = "LspReferenceHighlight",
+--   desc = "Clear highlights when entering insert mode",
+--   callback = function()
+--     vim.lsp.buf.clear_references()
+--   end,
+-- })
