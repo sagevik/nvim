1f37faef64ec2d89f820c8dc95b2e558f7ace9c5
sagevik <rsagevik@gmail.com>
2025-11-09 18:38:31 +0100

    update options

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,4 @@
 vim.opt.winborder = "rounded"
-vim.opt.swapfile = false
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
@@ -31,6 +30,7 @@ vim.opt.number = true         -- Make line numbers default
 vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.mouse = "a"           -- Enable mouse mode
 vim.opt.breakindent = true    -- Enable break indent
+vim.opt.swapfile = false      -- creates a swapfile
 vim.opt.undofile = true       -- Save undo history
 vim.opt.ignorecase = true     -- Case-insensitive searching UNLESS \C or capital in search
 vim.opt.smartcase = true      -- smart case
@@ -39,24 +39,23 @@ vim.opt.updatetime = 250      -- Decrease update time
 -- vim.opt.timeoutlen = 300      -- time to wait for a mapped sequence to complete (in milliseconds)
 -- vim.opt.backup = false -- creates a backup file
 -- vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
--- vim.opt.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience
--- vim.opt.termguicolors = true -- set termguicolors to enable highlight groups
+vim.opt.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience
+vim.opt.termguicolors = true             -- set termguicolors to enable highlight groups
 -- vim.opt.whichwrap = "bs<>[]hl" -- which "horizontal" keys are allowed to travel to prev/next line
-vim.opt.wrap = false      -- display lines as one long line
-vim.opt.linebreak = true  -- companion to wrap don't split words
-vim.opt.scrolloff = 4     -- minimal number of screen lines to keep above and below the cursor
-vim.opt.sidescrolloff = 8 -- minimal number of screen columns either side of cursor if wrap is `false`
-vim.opt.numberwidth = 2   -- set number column width to 2 {default 4}
-vim.opt.shiftwidth = 2    -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 2       -- insert n spaces for a tab
-vim.opt.softtabstop = 2   -- Number of spaces that a tab counts for while performing editing operations
-vim.opt.expandtab = true  -- convert tabs to spaces
-vim.opt.cursorline = true -- highlight the current line
-vim.opt.splitbelow = true -- force all horizontal splits to go below current window
-vim.opt.splitright = true -- force all vertical splits to go to the right of current window
--- vim.opt.swapfile = false -- creates a swapfile
+vim.opt.wrap = false                     -- display lines as one long line
+vim.opt.linebreak = true                 -- companion to wrap don't split words
+vim.opt.scrolloff = 4                    -- minimal number of screen lines to keep above and below the cursor
+vim.opt.sidescrolloff = 8                -- minimal number of screen columns either side of cursor if wrap is `false`
+vim.opt.numberwidth = 2                  -- set number column width to 2 {default 4}
+vim.opt.shiftwidth = 2                   -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2                      -- insert n spaces for a tab
+vim.opt.softtabstop = 2                  -- Number of spaces that a tab counts for while performing editing operations
+vim.opt.expandtab = true                 -- convert tabs to spaces
+vim.opt.cursorline = true                -- highlight the current line
+vim.opt.splitbelow = true                -- force all horizontal splits to go below current window
+vim.opt.splitright = true                -- force all vertical splits to go to the right of current window
 vim.opt.smarttab = true
-vim.opt.smartindent = true -- make indenting smarter again
+vim.opt.smartindent = true               -- make indenting smarter again
 -- vim.opt.showmode = false   -- we don't need to see things like -- INSERT -- anymore
 -- vim.opt.showtabline = 2 -- always show tabs
 -- vim.opt.backspace = "indent,eol,start" -- allow backspace on
@@ -84,9 +83,9 @@ vim.g.vimwiki_list = {
   },
   -- Optional: add more wikis
   -- {
-    -- path = "~/some/other/wiki/",
-    -- syntax = "markdown",
-    -- ext = ".md",
+  -- path = "~/some/other/wiki/",
+  -- syntax = "markdown",
+  -- ext = ".md",
   -- },
 }
 vim.g.vimwiki_global_ext = 1 -- apply wiki syntax to all files with .wiki/.md
