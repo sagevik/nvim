4c470155427618f396cc0550cf4c65a08429ee5a
sagevik <rsagevik@gmail.com>
2025-11-13 08:15:53 +0100

    update vimwiki settings

--- after/plugin/vimwiki.lua
+++ after/plugin/vimwiki.lua
@@ -1,5 +1,5 @@
 -- Advanced config (add after vim.pack.add)
-vim.g.vimwiki_ext2syntax = {  -- Map extensions to syntax (optional)
+vim.g.vimwiki_ext2syntax = { -- Map extensions to syntax (optional)
   ['.wiki'] = 'default',
   ['.md'] = 'markdown',
 }
@@ -9,19 +9,21 @@ vim.g.vimwiki_hl_headers = 1    -- Highlight headers in different colors
 vim.g.vimwiki_hl_cb_checked = 1 -- Highlight checked checkboxes
 
 -- Diary setup (for daily notes)
-vim.g.vimwiki_diary = 'diary'   -- Diary subfolder name
-vim.g.vimwiki_diary_weeks = 'full'  -- Full week view in diary index
+vim.g.vimwiki_diary = 'diary'      -- Diary subfolder name
+vim.g.vimwiki_diary_weeks = 'full' -- Full week view in diary index
 
 -- Keymaps (optional; customize <Leader>)
-local wk = vim.keymap.set  -- Or use which-key.nvim for a menu
-wk('n', '<Leader>ww', '<cmd>VimwikiIndex<CR>', { desc = 'Open wiki index' })
-wk('n', '<Leader>wd', '<cmd>VimwikiDiaryIndex<CR>', { desc = 'Open diary' })
-wk('n', '<Leader>we', '<cmd>VimwikiUIIndex<CR>', { desc = 'Wiki UI browser' })
-wk('n', '<Leader>ws', '<cmd>VimwikiUISelect<CR>', { desc = 'Select wiki' })
+vim.keymap.set('n', '<Leader>ww', '<cmd>VimwikiIndex<CR>', { desc = 'Open wiki index' })
+vim.keymap.set('n', '<Leader>wd', '<cmd>VimwikiDiaryIndex<CR>', { desc = 'Open diary' })
+vim.keymap.set('n', '<Leader>we', '<cmd>VimwikiUIIndex<CR>', { desc = 'Wiki UI browser' })
+vim.keymap.set('n', '<Leader>ws', '<cmd>VimwikiUISelect<CR>', { desc = 'Select wiki' })
+vim.keymap.set('n', '<Leader>wc', '<cmd>VimwikiGitCommit<CR>', { desc = 'Git commit wiki' })
+vim.keymap.set('n', '<Leader>wp', '<cmd>VimwikiGitPush<CR>', { desc = 'Git push wiki' })
+vim.keymap.set('n', '<C-x>', '<cmd>VimwikiToggleListItem<CR>', { desc = 'Toggle list item' })
 
 -- Auto-generate diary headers (Lua autocmd)
 vim.api.nvim_create_autocmd('BufNewFile', {
-  pattern = '*/diary/*.wiki',  -- Adjust for your ext/syntax
+  pattern = '*/diary/*.wiki', -- Adjust for your ext/syntax
   callback = function()
     local date = os.date('%Y-%m-%d')
     vim.api.nvim_buf_set_lines(0, 0, 0, false, { '# ' .. date })
